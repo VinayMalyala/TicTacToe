cfdd650a79e392d91d97d2a3bf3c819e8df822b0
Vinay Malyala <srivinay1432@gmail.com>
2024-07-04 14:24:49 +0530

    working on reset function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let audioTurn = new Audio('ting.mp3');
 let gameover = new Audio('gameover.mp3');
 
 let turn = 'X';
-
+let gameWin = false;
 
 // Function to change turn 
 const changeTurn = () => {
@@ -13,6 +13,25 @@ const changeTurn = () => {
 
 // Function to check for a win
 const checkWin = () => {
+    
+    boxtexts = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e => {
+        if ((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[0]].innerText !== '')) {
+            document.querySelector('.info').innerText = boxtexts[e[0]].innerText + ' Won';
+            gameWin = true;
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = '200px'
+        }
+    })
 
 }
 
@@ -26,7 +45,21 @@ Array.from(boxes).forEach(element => {
             turn = changeTurn();
             audioTurn.play();
             checkWin();
-            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            if (!gameWin){
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
         }
     })
+})
+
+// Add onclick listener to reset button
+reset.addEventListener('click', () => {
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = '';
+    })
+    turn = 'X';
+    gameWin = false;
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = '0px';
 })
